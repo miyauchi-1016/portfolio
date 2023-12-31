81f55e8ec5f5aaea02145fd6e651702cda927dcb
miyauchi-1016 <153565892+miyauchi-1016@users.noreply.github.com>
2023-12-31 16:29:24 +0900

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,40 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>動きのあるページ</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            background-color: #f0f0f0;
-        }
-
-        #animatedElement {
-            width: 100px;
-            height: 100px;
-            background-color: #3498db;
-            color: #ffffff;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            cursor: pointer;
-            transition: transform 0.3s ease-in-out;
-        }
-
-        #animatedElement:hover {
-            transform: scale(1.1);
-        }
-    </style>
 </head>
 <body>
-    <p>あ</p>
-    
+<p>あ</p>    
 </body>
 </html>
